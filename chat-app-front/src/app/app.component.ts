927357a113de245368a6a49e4d71c2b5c9c40e0c
Artem-Pliusnin <artemplyusnin2005@gmail.com>
2025-11-07 17:09:25 +0200

    Final UI commit

--- chat-app-front/src/app/app.component.ts
+++ chat-app-front/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthorizationService } from './services/authorization.service';
+import { Emitters } from './emitters/emitters';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'chat-app-front';
+export class AppComponent implements OnInit {
+  title = 'Chat';
+
+  private authService = inject(AuthorizationService);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    if (!this.authService.logIn()) {
+      this.router.navigate(['log-in']);
+    }
+  }
 }
